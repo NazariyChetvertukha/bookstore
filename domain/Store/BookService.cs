a00de20a2ad2d7282b5d42540206fd6ae10624cc
Nazariy Chetvertukha <carrrson2207@gmail.com>
2020-11-08 02:09:54 +0200

    Added and fixed

---
+++ domain/Store/BookService.cs
@@ -0,0 +1,16 @@
+﻿namespace Store
+{
+    public class BookService
+    {
+        private readonly IBookRepository bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        public  Book[] GetAllByQuery(string query)
+        {
+            return Book.IsIsbn(query) ? bookRepository.GetAllByIsdn(query) : bookRepository.GetAllByTitleOrAuthor(query);
+        }
+    }
+}

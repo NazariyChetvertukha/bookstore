f361efbd2d453d68e60a41573726a5596c006bf4
Nazariy Chetvertukha <carrrson2207@gmail.com>
2020-11-20 00:11:30 +0200

    Added new feature and cleanup

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -8,9 +8,9 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISDN 123-123456711", "D. Kohut", "Art pf Programming"),
-            new Book(2, "ISDN 123-123456712", "S. Something", "Refactoring"),
-            new Book(3, "ISDN 123-123456713", "L. Stek","C Programming Language")
+            new Book(1, "ISDN 123-123456711", "D. Kohut", "Art pf Programming", "Description for a great book!",17.5m),
+            new Book(2, "ISDN 123-123456712", "S. Something", "Refactoring", "Description for a perfect book!",15m),
+            new Book(3, "ISDN 123-123456713", "L. Stek","C Programming Language","Description for a super book!",10m)
         };
 
         public Book[] GetAllByTitle(string titlePart)
@@ -30,6 +30,12 @@ namespace Store.Memory
             return books.Where(book => book.Author.Contains(query)
                                     || book.Title.Contains(query))
                 .ToArray();
+        }
+        
+        public Book GetById(in int id)
+        {
+            var i = id;
+            return books.Single(book => book.Id == i);
         }
     }
 }
